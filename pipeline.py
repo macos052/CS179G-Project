6d39daab95d67713ad6d75cf34f8100fc63981ae
Matthew Acosta <macos052@ucr.edu>
2026-08-13 14:33:22 -0700

    Add SQL query to extract hashtags from posts

--- pipeline.py
+++ pipeline.py
@@ -7,3 +7,17 @@ spark = SparkSession.builder \
 df = spark.read.json("posts_deduped.jsonl")
 
 df.createOrReplaceTempView("posts")
+
+# Extract hashtags
+hashtags = spark.sql("""
+  SELECT 
+    p.uri,
+    TO_DATE(p.record.createdAt) AS post_date,
+    LOWER(feature.tag) AS hashtag
+  FROM posts p
+  LATERAL VIEW explode(record.facets) AS facet
+  LATERAL VIEW explode(facet.features) AS feature
+  WHERE feature.`$type` = 'app.bsky.richtext.facet#tag'
+""")
+
+hashtags.cache()
