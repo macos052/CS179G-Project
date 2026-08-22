8ad213168b572671fce64a5b24ac6532ebc4333d
Matthew Acosta <macos052@ucr.edu>
2026-08-21 23:12:33 -0700

    Add database connection pool configuration using mysql2

---
+++ db.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10
+});
+
+module.exports = pool;
